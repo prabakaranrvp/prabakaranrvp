a9618051a6400f45528bc8dd94bc63875b6a4d03
Prabakaran Raja <prabakaranrvp@gmail.com>
2021-03-03 00:17:00 +0530

    Cool Font Style Animations

---
+++ script.js
@@ -0,0 +1,48 @@
+var unusedClasses = ['akaya', 'roboto', 'noto', 'lat', 'stick', 'roboto-condensed', 'pt-sans', 'source-code', 'anton', 'lobster', 'dancing', 'architects', 'indie'];
+var usedClasses   = [];
+
+var pClasses = unusedClasses;
+var pUsed    = [];
+
+var intervalTime  = 500;
+
+(function() {
+
+  Math.__proto__.randomIndex = function(max) {
+    return this.floor(this.random() * max) + 1;
+  }
+
+  setTimeout(function() {
+
+    var h = document.getElementsByTagName('h1')[0];
+    var p = document.getElementsByTagName('p')[0];
+
+    setInterval(function() {
+
+      if(unusedClasses.length === 0) {
+        unusedClasses = usedClasses;
+        usedClasses = [];
+      }
+      randomIndex = Math.randomIndex(unusedClasses.length-2);
+      h.className = unusedClasses[randomIndex];
+      usedClasses.push(unusedClasses.splice(randomIndex, 1)[0]);
+    }, intervalTime);
+
+    setTimeout(function () {
+
+      setInterval(function() {
+        if(pClasses.length === 0) {
+          pClasses = pUsed;
+          pUsed = [];
+        }
+        randomIndex = Math.randomIndex(pClasses.length-2);
+        p.className = pClasses[randomIndex];
+        pUsed.push(pClasses.splice(randomIndex, 1)[0]);
+      }, intervalTime);
+
+    }, intervalTime/2)
+    
+
+  }, 0);
+
+})();
