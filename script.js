efe2de0036f0e40e8c217ac13dc016dcf12908b8
Prabakaran Raja <prabakaranrvp@gmail.com>
2021-03-03 12:03:54 +0530

    Timout added, Style updates

--- script.js
+++ script.js
@@ -6,51 +6,68 @@ var pUsed = [];
 
 var intervalTime = 1200;
 
-(function () {
 
-  Math.__proto__.randomIndex = function (max) {
-    return this.floor(this.random() * max) + 1;
-  }
+/*
+  Does Animation for the Fonts
+*/
+function initFontAnime() {
+  if(document.getElementsByTagName('body')[0].className !== 'even')
+    return;
 
+  var h = document.getElementsByTagName('h1')[0];
+  var p = document.getElementsByTagName('p')[0];
+
+  updateFont(unusedClasses, usedClasses, h);
   setTimeout(function () {
+    updateFont(pClasses, pUsed, p);
+  }, intervalTime / 2)
 
-    var h = document.getElementsByTagName('h1')[0];
-    var p = document.getElementsByTagName('p')[0];
+};
 
-    setInterval(function () {
+// Updates the font for the given element in a sequential interval
+function updateFont(mainClass, subClass, el) {
+  setInterval(function () {
+    if (mainClass.length === 0) {
+      mainClass = subClass;
+      subClass = [];
+    }
+    randomIndex = Math.randomIndex(mainClass.length - 2);
+    el.className = mainClass[randomIndex];
+    subClass.push(mainClass.splice(randomIndex, 1)[0]);
+  }, intervalTime);
+}
 
-      if (unusedClasses.length === 0) {
-        unusedClasses = usedClasses;
-        usedClasses = [];
-      }
-      randomIndex = Math.randomIndex(unusedClasses.length - 2);
-      h.className = unusedClasses[randomIndex];
-      usedClasses.push(unusedClasses.splice(randomIndex, 1)[0]);
-    }, intervalTime);
+Math.__proto__.randomIndex = function (max) {
+  return this.floor(this.random() * max) + 1;
+}
 
-    setTimeout(function () {
+// Fetch with Timout function
+function timout(url, timerMs) {
+  return new Promise(function(resolve, reject) {
+    var timer = setTimeout(function() {
+      console.log('rejecting from timer')
+      reject('timeout');
+    }, timerMs || 2000);
 
-      setInterval(function () {
-        if (pClasses.length === 0) {
-          pClasses = pUsed;
-          pUsed = [];
-        }
-        randomIndex = Math.randomIndex(pClasses.length - 2);
-        p.className = pClasses[randomIndex];
-        pUsed.push(pClasses.splice(randomIndex, 1)[0]);
-      }, intervalTime);
+    fetch(url).then(function (resp) {
+      resp.json().then(function (data) {
+        clearTimeout(timer);
+        resolve(data);
+      }).catch(function() {
+        reject(...arguments);
+        clearTimeout(timer);
+      })
+    });
+  });
+}
 
-    }, intervalTime / 2)
 
-  }, 0);
-
-})();
-
-fetch('https://api.ipify.org?format=json').then(function (resp) {
-  resp.json().then(function (data) {
-    var lastAddr = data.ip.split('.').pop();
-    document.getElementsByTagName('body')[0].className =  (lastAddr % 2 == 0) ? 'even' : 'odd'
-  }).catch(function() {
-    document.getElementsByTagName('body')[0].className = 'even';
-  })
+// Main Init function
+timout('https://api.ipify.org?format=json', 3000).then(function(data) {
+  var lastAddr = data.ip.split('.').pop();
+  document.getElementsByTagName('body')[0].className = (lastAddr % 2 == 0) ? 'even' : 'odd'
+  initFontAnime();
+}).catch(function() {
+  console.error(arguments);
+  document.getElementsByTagName('body')[0].className = 'odd';
 })
